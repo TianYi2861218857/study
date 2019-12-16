67b7b33b31eedd0f92314c42c8a49eb8080afed4
TianYi2861218857 <2861218857@qq.com>
2019-12-16 16:57:44 +0800

    19/12/16/cs

---
+++ 001-server1.js
@@ -0,0 +1,36 @@
+/*
+    1.搭建一个基本的服务器
+    2.读取本地资源文件
+*/
+const http = require('http')
+const fs = require('fs')
+// console.log(http)
+
+const server = http.createServer(function(req,res){
+	/* //基本服务器
+	//改变状态码
+	res.statusCode = 200;
+	//向前台返回内容
+	res.end('Hello World\n')
+	// console.log(server)
+	*/
+	// console.log(req.url)
+	const urlStr = req.url
+	if(urlStr == './favicon.ico'){
+		res.end('favicon.ico')
+	}
+	const filePath = './'+ urlStr
+	fs.readFile(filePath,function(err,data){
+		if(!err){
+			res.end(data)
+		}else{
+			res.statusCode = 404
+			res.end('not found...')
+		}
+	})
+
+})
+
+server.listen(3000,'127.0.0.1',()=>{
+	console.log('server running at http://127.0.0.1:3000')
+})
---
+++ 002-server2.js
@@ -0,0 +1,28 @@
+/*
+    1.搭建一个基本的服务器
+    2.读取本地资源文件
+*/
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer(function(req,res){
+	console.log(req.method)
+	const urlStr = req.url
+	if(urlStr == './favicon.ico'){
+		res.end('favicon.ico')
+	}
+	const filePath = './'+ urlStr
+	fs.readFile(filePath,function(err,data){
+		if(!err){
+			res.end(data)
+		}else{
+			res.statusCode = 404
+			res.end('not found...')
+		}
+	})
+
+})
+
+server.listen(3000,'127.0.0.1',()=>{
+	console.log('server running at http://127.0.0.1:3000')
+})
